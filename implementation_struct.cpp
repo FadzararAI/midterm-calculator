6345414250643bfb653918560e07ee6ec44da318
FadzararAI <fadzarar@gmail.com>
2022-10-12 13:11:33 +0700

    Added sorting functionality.

--- implementation_struct.cpp
+++ implementation_struct.cpp
@@ -18,6 +18,27 @@ void add(int x){
 		rear=temp;
 	}
 }
+void sortll(){
+    struct Node *current = front, *index = NULL;
+    int temp;
+    if(front == NULL){
+        return;
+    }
+    else{
+        while(current != NULL){
+            index = current->next;
+            while(index != NULL){
+                if(current->number > index->number){
+                    temp = current->number;
+                    current->number = index->number;
+                    index->number = temp;
+                }
+                index = index->next;
+            }
+            current = current->next;
+        }
+    }
+}
 void history(){
 	if(front==NULL){
 		cout << "List Empty" << endl;
@@ -32,8 +53,13 @@ void history(){
 }
 int main(){
 	add(5);
+	add(20);
 	add(10);
+	add(25);
+	add(60);
 	add(50);
+	history();
+	sortll();
 	history();
 	return 0;
 }
