dab41205e6ca1dc6dca7bcce7f74e1f72098ba77
FadzararAI <fadzarar@gmail.com>
2022-10-14 22:43:39 +0700

    Merge Queue

---
+++ Queue.h
@@ -0,0 +1,28 @@
+#ifndef QUEUEH
+#define QUEUEH
+#include<iostream>
+#include<stdlib.h>
+#include<cmath>
+#define MAX_SIZE 101  
+using namespace std;
+
+class Queue
+{
+private:
+	int A[MAX_SIZE];
+	int front, rear; 
+public:
+	bool IsEmpty();
+	bool IsFull();
+	void Enqueue(int x); 
+	void Dequeue();
+	int Front();
+		Queue()
+		{
+		front = -1; 
+		rear = -1;
+		}
+
+};
+#endif
+
